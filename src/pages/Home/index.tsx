889f0b2b24a1e323a853d991d9d4159d6c8cb04d
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-28 14:12:54 +0200

    feat: vector Icon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, Button, SafeAreaView } from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 import { HomeProps } from "../../typings";
 
@@ -7,6 +8,7 @@ export const Home = ({ navigation }: HomeProps) => {
     return (
         <SafeAreaView>
             <Text>Home</Text>
+            <FontAwesome5 name="comments" size={20} />
             {/* <Button
                 title="Go to Details"
                 onPress={() => navigation.navigate("Product")}
