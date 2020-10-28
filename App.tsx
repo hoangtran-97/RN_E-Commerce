5a9ef11c26587f431955c91b11fa1e9590379844
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-28 15:59:35 +0200

    feat: context color

--- App.tsx
+++ App.tsx
@@ -1,62 +1,13 @@
 import "react-native-gesture-handler";
-import React, { useContext } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import React from "react";
 
 import { ThemeProvider } from "./src/context";
-import { Home } from "./src/pages/Home";
-import { Setting } from "./src/pages/Setting";
-import { Cart } from "./src/pages/Cart";
-import { ThemeContext } from "./src/context";
-
-const Tab = createBottomTabNavigator();
+import { Navigation } from "./navigation";
 
 const App = () => {
-    const { theme } = useContext(ThemeContext);
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: theme.foreground,
-        },
-    });
     return (
         <ThemeProvider>
-            <SafeAreaView style={styles.container}>
-                <NavigationContainer>
-                    <Tab.Navigator
-                        screenOptions={({ route }) => ({
-                            tabBarIcon: ({ color, size }) => {
-                                let iconName = "question";
-
-                                if (route.name === "Home") {
-                                    iconName = "home";
-                                } else if (route.name === "Setting") {
-                                    iconName = "user-cog";
-                                } else if (route.name === "Cart") {
-                                    iconName = "shopping-cart";
-                                }
-                                return (
-                                    <FontAwesome5
-                                        name={iconName}
-                                        size={size}
-                                        color={color}
-                                    />
-                                );
-                            },
-                        })}
-                        tabBarOptions={{
-                            activeTintColor: theme.text,
-                            inactiveTintColor: theme.background,
-                            style: { backgroundColor: theme.foreground },
-                        }}>
-                        <Tab.Screen name="Home" component={Home} />
-                        <Tab.Screen name="Cart" component={Cart} />
-                        <Tab.Screen name="Setting" component={Setting} />
-                    </Tab.Navigator>
-                </NavigationContainer>
-            </SafeAreaView>
+            <Navigation />
         </ThemeProvider>
     );
 };
