61ea0e25457712ffe7ce399f29a4742a26785cc3
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-28 14:45:05 +0200

    feat: basic nav

--- App.tsx
+++ App.tsx
@@ -2,19 +2,45 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 import { Home } from "./src/pages/Home";
-import { Product } from "./src/pages/Product";
-import { RootStackParamList } from "./src/typings";
+import { Setting } from "./src/pages/Setting";
+import { Cart } from "./src/pages/Cart";
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color, size }) => {
+                        let iconName = "question";
+
+                        if (route.name === "Home") {
+                            iconName = "home";
+                        } else if (route.name === "Setting") {
+                            iconName = "user-cog";
+                        } else if (route.name === "Cart") {
+                            iconName = "shopping-cart";
+                        }
+                        return (
+                            <FontAwesome5
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: "tomato",
+                    inactiveTintColor: "gray",
+                }}>
                 <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Product" component={Product} />
+                <Tab.Screen name="Cart" component={Cart} />
+                <Tab.Screen name="Setting" component={Setting} />
             </Tab.Navigator>
         </NavigationContainer>
     );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, Button, SafeAreaView } from "react-native";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 import { HomeProps } from "../../typings";
 
@@ -8,7 +7,6 @@ export const Home = ({ navigation }: HomeProps) => {
     return (
         <SafeAreaView>
             <Text>Home</Text>
-            <FontAwesome5 name="comments" size={20} />
             {/* <Button
                 title="Go to Details"
                 onPress={() => navigation.navigate("Product")}
