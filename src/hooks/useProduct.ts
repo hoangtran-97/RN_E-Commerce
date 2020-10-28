cd8f1e50d9b9c26d6d0214a3eb7eed75dc6ca70c
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-28 17:32:38 +0200

    feat: android local host

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
+import { Platform } from "react-native";
 
 import { Product } from "../typings";
 
 export const useProduct = () => {
     const [data, setData] = useState<Product[]>([]);
     useEffect(() => {
+        const URL =
+            Platform.OS === "ios"
+                ? "http://localhost:3001/api/v1/products"
+                : "http://10.0.2.2:3001/api/v1/products";
         const loadData = async () => {
             try {
-                const response = await fetch(
-                    "http://localhost:3001/api/v1/products",
-                );
+                const response = await fetch(URL);
                 const json = await response.json();
                 setData(json);
             } catch (error) {}
