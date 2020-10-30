2a7b79d5d24c4352f7a6bee46323795cd6bcdb41
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-30 12:28:23 +0200

    feat: cart swipe

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -7,6 +7,7 @@ import {
     TouchableOpacity,
     Image,
     FlatList,
+    Alert,
     ImageBackground,
 } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
@@ -27,17 +28,54 @@ export const Cart = () => {
     const { theme } = useContext(ThemeContext);
     const { inCart } = useSelector((state: AppState) => state.product);
     const textStyle = { ...styles.text, color: theme.text };
-    const renderRightActions = (progress: any, dragX: any) => {
-        const trans = dragX.interpolate({
-            inputRange: [0, 50, 100, 101],
-            outputRange: [-20, 0, 0, 1],
+    // const renderRightActions = (progress: any, dragX: any) => {
+    //     const trans = dragX.interpolate({
+    //         inputRange: [0, 50, 100, 101],
+    //         outputRange: [-20, 0, 0, 1],
+    //     });
+    //     return (
+    //         <TouchableOpacity
+    //             style={{ transform: [{ translateX: 0 }], width: 192 }}>
+    //             <Animated.Text
+    //                 style={[
+    //                     styles.actionText,
+    //                     {
+    //                         transform: [{ translateX: trans }],
+    //                     },
+    //                 ]}>
+    //                 Remove from cart
+    //             </Animated.Text>
+    //         </TouchableOpacity>
+    //     );
+    // };
+    const renderRightAction = (
+        text: string,
+        color: string,
+        x: number,
+        progress: any,
+    ) => {
+        const trans = progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [x, 0],
         });
+        const pressHandler = () => {
+            Alert.alert(text);
+        };
         return (
-            <RectButton>
-                <Animated.Text>Archive</Animated.Text>
-            </RectButton>
+            <Animated.View style={{ flex: 1, transform: [{ translateX: 0 }] }}>
+                <RectButton
+                    style={[styles.rightAction, { backgroundColor: color }]}
+                    onPress={pressHandler}>
+                    <Text style={styles.actionText}>{text}</Text>
+                </RectButton>
+            </Animated.View>
         );
     };
+    const renderRightActions = (progress: any) => (
+        <View style={styles.rightActions}>
+            {renderRightAction("More", "#dd2c00", 64, progress)}
+        </View>
+    );
 
     const renderItem = ({ item }: { item: Product }) => (
         <Swipeable renderRightActions={renderRightActions}>
@@ -48,11 +86,13 @@ export const Cart = () => {
                 source={{
                     uri: `${item.img}`,
                 }}>
-                <Text style={textStyle}>{item.name}</Text>
-                <Text style={textStyle}>
-                    Size: {item.sizes} - Variant: {item.variants}
-                </Text>
-                <Text style={textStyle}>Price: {item.price} EUR</Text>
+                <View style={styles.imgOverlay}>
+                    <Text style={textStyle}>{item.name}</Text>
+                    <Text style={textStyle}>
+                        Size: {item.sizes} - Variant: {item.variants}
+                    </Text>
+                    <Text style={textStyle}>Price: {item.price} EUR</Text>
+                </View>
             </ImageBackground>
         </Swipeable>
     );
@@ -79,8 +119,6 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         width: "100%",
         height: 100,
-
-        justifyContent: "center",
     },
     container__empty: {
         marginTop: 20,
@@ -89,5 +127,32 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    imageStyle: { opacity: 0.5 },
+    imageStyle: {},
+    leftAction: {
+        flex: 1,
+        backgroundColor: "#497AFC",
+        justifyContent: "center",
+    },
+    actionText: {
+        color: "white",
+        fontSize: 16,
+        backgroundColor: "transparent",
+        padding: 10,
+    },
+    rightAction: {
+        height: 100,
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+    },
+    rightActions: {
+        width: 192,
+        height: 100,
+        flexDirection: "row",
+    },
+    imgOverlay: {
+        backgroundColor: "rgba(66, 66, 66, 0.5)",
+        flex: 1,
+        justifyContent: "center",
+    },
 });
