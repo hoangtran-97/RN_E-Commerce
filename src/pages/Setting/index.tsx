09d49a34b8068c0b916cdea0d181cd64f223d665
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-11-01 15:29:10 +0200

    feat: google signin iOS

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,19 +1,28 @@
 import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 import { Text, SafeAreaView, View, StyleSheet } from "react-native";
 
 import { ColorSwitcher } from "../../Components/ColorSwitcher";
 import SettingGoogleSignin from "../../Components/SettingGoogleSignin";
 import { ThemeContext } from "../../context";
+import { AppState } from "../../typings";
 
 export const Setting = () => {
     const { theme } = useContext(ThemeContext);
+    const { currentUser, token } = useSelector((state: AppState) => state.user);
+    const userIsSignedin = token.length > 0 ? true : false;
+    console.log("signin status", userIsSignedin);
+
     return (
         <View
-            style={{
-                ...styles.container,
-                backgroundColor: theme.foreground,
-            }}>
-            <SettingGoogleSignin />
+            style={{ ...styles.container, backgroundColor: theme.foreground }}>
+            {!userIsSignedin ? (
+                <SettingGoogleSignin />
+            ) : (
+                <View>
+                    <Text>Test</Text>
+                </View>
+            )}
             <ColorSwitcher />
         </View>
     );
