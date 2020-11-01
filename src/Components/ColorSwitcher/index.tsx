01c3e261f48b61d1a21a6e5c75c26370c26148e7
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-11-01 21:12:12 +0200

    feat: animation working

--- src/Components/ColorSwitcher/index.tsx
+++ src/Components/ColorSwitcher/index.tsx
@@ -1,9 +1,65 @@
-import React, { useContext } from "react";
-import { Button } from "react-native";
+import React, { useContext, useState, useRef } from "react";
+import { Button, Animated, Easing, Text } from "react-native";
+// import Animated, { Easing } from "react-native-reanimated";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 import { ThemeContext } from "../../context";
 
 export const ColorSwitcher = () => {
     const { switchTheme } = useContext(ThemeContext);
-    return <Button onPress={switchTheme} title="Swith theme" color="#5AC8FA" />;
+    const [spinning, setSpinning] = useState(false);
+    const spinValue = useRef(new Animated.Value(0)).current;
+    // Animated.timing(spinValue, {
+    //     toValue: spinning ? 1 : 0,
+    //     duration: 300,
+    //     easing: Easing.linear, // Easing is an additional import from react-native
+    //     useNativeDriver: true,
+    // }).start();
+    const spin = spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["0deg", "180deg"],
+    });
+    const spinIn = () => {
+        Animated.timing(spinValue, {
+            toValue: 0,
+            duration: 300,
+            easing: Easing.linear, // Easing is an additional import from react-native
+            useNativeDriver: true,
+        }).start();
+    };
+
+    const spinOut = () => {
+        Animated.timing(spinValue, {
+            toValue: 1,
+            duration: 300,
+            easing: Easing.linear, // Easing is an additional import from react-native
+            useNativeDriver: true,
+        }).start();
+    };
+    const animation = () => {
+        if (spinning) {
+            spinIn();
+            setSpinning((prev) => !prev);
+        } else {
+            spinOut();
+            setSpinning((prev) => !prev);
+        }
+    };
+
+    return (
+        <>
+            <Animated.View style={{ transform: [{ rotate: spin }] }}>
+                <FontAwesome5 name="moon" size={50} />
+            </Animated.View>
+            <Text>{spinning ? "spinIN" : "spinOut"}</Text>
+            <Button
+                onPress={() => {
+                    animation();
+                    switchTheme();
+                }}
+                title="Swith theme"
+                color="#5AC8FA"
+            />
+        </>
+    );
 };
