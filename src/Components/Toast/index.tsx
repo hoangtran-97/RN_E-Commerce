e621eeb92b2f8e653a82f16742e5cd1f9e568b5c
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-11-03 09:53:38 +0200

    fix: toast

--- src/Components/Toast/index.tsx
+++ src/Components/Toast/index.tsx
@@ -7,14 +7,15 @@ import { AppState } from "../../typings";
 
 export const ToastMessageRedux = () => {
     const toastState = useSelector((state: AppState) => state.ui.toast);
-    // const { clearMessages } = useDispatch<Dispatch>().global;
     const { message, intent } = toastState;
-    console.log("toasted");
 
     const { toast } = useToast();
     useEffect(() => {
         if (message) {
-            toast({ message, intent });
+            if (message.length > 0) {
+                console.log("toasted");
+                toast({ message, intent });
+            }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [message]);
